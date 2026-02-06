7ead282987b18804e0d6d3790e95971ec920b24a
Gustavo <sgkenzo212007@gmail.com>
2026-02-06 18:08:46 -0300

    feat: implements repository to usuario, topico and resposta

---
+++ src/main/java/br/com/gustavo/forum_hub/domain/topico/TopicoRepository.java
@@ -0,0 +1,6 @@
+package br.com.gustavo.forum_hub.domain.topico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
