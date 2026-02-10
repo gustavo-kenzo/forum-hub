77712d7f6245591de8b3ea1439cc99550a6f37f1
Gustavo <sgkenzo212007@gmail.com>
2026-02-10 11:19:39 -0300

    feat: implements resposta endpoints with registration and solution management
    - add RespostaController - add RespostaService - input/output DTOs with validations - POST /resposta - PUT/DELETE /resposta/{id}/solucao - authorization and business rules (without Spring Security)

---
+++ src/main/java/br/com/gustavo/forum_hub/controller/RespostaController.java
@@ -0,0 +1,48 @@
+package br.com.gustavo.forum_hub.controller;
+
+import br.com.gustavo.forum_hub.domain.resposta.DadosCadastrarResposta;
+import br.com.gustavo.forum_hub.domain.resposta.DadosDetalhamentoResposta;
+import br.com.gustavo.forum_hub.service.RespostaService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("resposta")
+@AllArgsConstructor
+public class RespostaController {
+
+    private RespostaService respostaService;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrarResposta(@RequestBody @Valid DadosCadastrarResposta dados, UriComponentsBuilder uriBuilder) {
+        var resposta = respostaService.cadastrar(dados);
+        var uri = uriBuilder.path("/resposta/{id}").buildAndExpand(resposta.getId()).toUri();
+
+        var autor = resposta.getAutor().getNome();
+        var topico = resposta.getTopico().getTitulo();
+
+        var dto = new DadosDetalhamentoResposta(resposta.getMensagem(), topico, autor, resposta.getDataCriacao(), resposta.getSolucao());
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{respostaId}/solucao")
+    @Transactional
+    public ResponseEntity marcarComoSolucao(@PathVariable Long respostaId, @RequestParam Long usuarioId) {
+        respostaService.definirSolucao(respostaId, usuarioId, true);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{respostaId}/solucao")
+    @Transactional
+    public ResponseEntity desmarcarComoSolucao(@PathVariable Long respostaId, @RequestParam Long usuarioId) {
+        respostaService.definirSolucao(respostaId, usuarioId, false);
+        return ResponseEntity.noContent().build();
+    }
+
+}
